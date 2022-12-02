b51d6b925fc74588ca607758087745e0dacc8ab9
elianDharmawan <mishael.elian@ti.ukdw.ac.id>
2022-12-02 21:42:37 +0700

    tambah sqldump

---
+++ pelanggan6.sql
@@ -0,0 +1,6 @@
+INSERT INTO PELANGGAN ( LOKASI_ID, PELANGGAN_ID, NAMA_LENGKAP, KOTA, PROPINSI, EMAIL, GENDER, STATUS, USERNAME ) VALUES ( 6415, 401554, 'Carie Spooner', 'Hodge', 'GA', 'CSpooner@wzzryyiptkhrfah.edu', 'F', 'I', ''); 
+INSERT INTO PELANGGAN ( LOKASI_ID, PELANGGAN_ID, NAMA_LENGKAP, KOTA, PROPINSI, EMAIL, GENDER, STATUS, USERNAME ) VALUES ( 2336, 401563, 'Lance Lindeman', 'Baldwin Lake', 'NM', 'LLindeman@holzwfnlcnrquhg.edu', 'F', 'I', ''); 
+INSERT INTO PELANGGAN ( LOKASI_ID, PELANGGAN_ID, NAMA_LENGKAP, KOTA, PROPINSI, EMAIL, GENDER, STATUS, USERNAME ) VALUES ( 7719, 401572, 'Margit Fulkerson', 'Del Rosa', 'OH', 'MFulkerson@lmlfbnhwjaqqwva.edu', 'F', NULL, ''); 
+INSERT INTO PELANGGAN ( LOKASI_ID, PELANGGAN_ID, NAMA_LENGKAP, KOTA, PROPINSI, EMAIL, GENDER, STATUS, USERNAME ) VALUES ( 9644, 901574, 'Jan Busse', 'Victorville', 'MS', 'JBusse@jwfzwrjymqitgom.mil', 'F', 'I', ''); 
+INSERT INTO PELANGGAN ( LOKASI_ID, PELANGGAN_ID, NAMA_LENGKAP, KOTA, PROPINSI, EMAIL, GENDER, STATUS, USERNAME ) VALUES ( 6415, 201595, 'Charlene Keeton', 'Rainbow Wells', 'NC', 'CKeeton@kxqrzpvxdtzicta.gov', 'F', 'I', ''); 
+commit;
