c9b6222b0c91bd3f68e78920c9c9c28809dfeb0b
Elian-Dharmawan <89970962+Elian-Dharmawan@users.noreply.github.com>
2022-12-08 15:56:21 +0700

    Update pelanggan6.sql
    add username values

--- pelanggan6.sql
+++ pelanggan6.sql
@@ -1,6 +1,6 @@
-INSERT INTO PELANGGAN ( LOKASI_ID, PELANGGAN_ID, NAMA_LENGKAP, KOTA, PROPINSI, EMAIL, GENDER, STATUS, USERNAME ) VALUES ( 6415, 401554, 'Carie Spooner', 'Hodge', 'GA', 'CSpooner@wzzryyiptkhrfah.edu', 'F', 'I', ''); 
-INSERT INTO PELANGGAN ( LOKASI_ID, PELANGGAN_ID, NAMA_LENGKAP, KOTA, PROPINSI, EMAIL, GENDER, STATUS, USERNAME ) VALUES ( 2336, 401563, 'Lance Lindeman', 'Baldwin Lake', 'NM', 'LLindeman@holzwfnlcnrquhg.edu', 'F', 'I', ''); 
-INSERT INTO PELANGGAN ( LOKASI_ID, PELANGGAN_ID, NAMA_LENGKAP, KOTA, PROPINSI, EMAIL, GENDER, STATUS, USERNAME ) VALUES ( 7719, 401572, 'Margit Fulkerson', 'Del Rosa', 'OH', 'MFulkerson@lmlfbnhwjaqqwva.edu', 'F', NULL, ''); 
-INSERT INTO PELANGGAN ( LOKASI_ID, PELANGGAN_ID, NAMA_LENGKAP, KOTA, PROPINSI, EMAIL, GENDER, STATUS, USERNAME ) VALUES ( 9644, 901574, 'Jan Busse', 'Victorville', 'MS', 'JBusse@jwfzwrjymqitgom.mil', 'F', 'I', ''); 
-INSERT INTO PELANGGAN ( LOKASI_ID, PELANGGAN_ID, NAMA_LENGKAP, KOTA, PROPINSI, EMAIL, GENDER, STATUS, USERNAME ) VALUES ( 6415, 201595, 'Charlene Keeton', 'Rainbow Wells', 'NC', 'CKeeton@kxqrzpvxdtzicta.gov', 'F', 'I', ''); 
+INSERT INTO PELANGGAN ( LOKASI_ID, PELANGGAN_ID, NAMA_LENGKAP, KOTA, PROPINSI, EMAIL, GENDER, STATUS, USERNAME ) VALUES ( 6415, 401554, 'Carie Spooner', 'Hodge', 'GA', 'CSpooner@wzzryyiptkhrfah.edu', 'F', 'I', 'Carlie'); 
+INSERT INTO PELANGGAN ( LOKASI_ID, PELANGGAN_ID, NAMA_LENGKAP, KOTA, PROPINSI, EMAIL, GENDER, STATUS, USERNAME ) VALUES ( 2336, 401563, 'Lance Lindeman', 'Baldwin Lake', 'NM', 'LLindeman@holzwfnlcnrquhg.edu', 'F', 'I', 'Lance'); 
+INSERT INTO PELANGGAN ( LOKASI_ID, PELANGGAN_ID, NAMA_LENGKAP, KOTA, PROPINSI, EMAIL, GENDER, STATUS, USERNAME ) VALUES ( 7719, 401572, 'Margit Fulkerson', 'Del Rosa', 'OH', 'MFulkerson@lmlfbnhwjaqqwva.edu', 'F', NULL, 'Margit'); 
+INSERT INTO PELANGGAN ( LOKASI_ID, PELANGGAN_ID, NAMA_LENGKAP, KOTA, PROPINSI, EMAIL, GENDER, STATUS, USERNAME ) VALUES ( 9644, 901574, 'Jan Busse', 'Victorville', 'MS', 'JBusse@jwfzwrjymqitgom.mil', 'F', 'I', 'Jan'); 
+INSERT INTO PELANGGAN ( LOKASI_ID, PELANGGAN_ID, NAMA_LENGKAP, KOTA, PROPINSI, EMAIL, GENDER, STATUS, USERNAME ) VALUES ( 6415, 201595, 'Charlene Keeton', 'Rainbow Wells', 'NC', 'CKeeton@kxqrzpvxdtzicta.gov', 'F', 'I', 'Charlene'); 
 commit;
